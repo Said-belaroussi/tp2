162f699e28a1bdfcef14da0d1914c47094cd8328
Said-belaroussi <said.belaroussi@ulb.be>
2022-02-24 10:16:59 +0100

    Titre du commit
    Co-authored-by: Said Belaroussi <said.belaroussi@ulb.be>

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,3 +1,42 @@
-public class Exo2 {
+public class Fraction {
+    int numerator;
+    int denominator;
+    public Fraction(int a, int b) {
+        numerator = a;
+        denominator = b;
+    }
+    static public Fraction somme(Fraction f, Fraction b) {
+        int pgcd = pgcd(f.getDenominator(), b.getDenominator());
+        Fraction res = new Fraction((f.getNumerator()*pgcd/f.getDenominator()) + (b.getNumerator()*pgcd/b.getDenominator()), pgcd);
+        return res;
+    }
+    public void printFraction() {
+        System.out.print("f");
+    }
 
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    private static int pgcd(int n1, int n2){
+        int pgcd = 0;
+        for(int i=1; i <= n1 && i <= n2; i++) {
+            if(n1% i==0 && n2%i==0){
+                pgcd = i;
+            }
+        }
+        return pgcd;
+    }
 }
--- test/FractionTest.java
+++ test/FractionTest.java
@@ -1,4 +1,17 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class FractionTest {
-  
+
+
+    @Test
+    public void testSomme() {
+        Fraction a = new Fraction(1,3);
+        Fraction b = new Fraction(1,2);
+        Fraction res = Fraction.somme(a,b);
+        assertEquals(5, res.getNumerator());
+        assertEquals(6, res.getDenominator());
+    }
+
 }
